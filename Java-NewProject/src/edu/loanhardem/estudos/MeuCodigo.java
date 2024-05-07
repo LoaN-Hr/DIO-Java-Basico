05b04252da4323bdf617fb289630b04d56fabc39
loan <loanhardem14@hotmail.com>
2024-05-07 17:01:49 -0300

    estudos 0.1v

---
+++ Java-NewProject/src/edu/loanhardem/estudos/MeuCodigo.java
@@ -0,0 +1,14 @@
+package edu.loanhardem.estudos;
+
+public class MeuCodigo {
+    public static void main(String[] args) {
+       
+        String meuNome = "Loan Hardem";
+        int anoNascimento = 2005;
+        boolean estouNamorando = false;
+
+        System.out.println("Nome: " + meuNome);
+        System.out.println("Data de Nascimento: " + anoNascimento);
+        System.out.println("Estado civil: " + estouNamorando);
+    }
+}
